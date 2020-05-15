5935e877fedd19f657d95c77712ce664ad90d3fe
kadrianne <kristine.a.du@gmail.com>
2020-05-15 13:52:00 -0600

    Add debugger.

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 import reducers from './reducers'
 import AppMain from './AppMain'
 
-const store = createStore(reducers)
+const store = createStore(reducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 export default function App() {
 

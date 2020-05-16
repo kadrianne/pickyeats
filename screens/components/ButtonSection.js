7cecfb05cf921a1445019e0915444daf823baf96
kadrianne <kristine.a.du@gmail.com>
2020-05-15 22:36:06 -0600

    Fix if no price available.

--- screens/components/ButtonSection.js
+++ screens/components/ButtonSection.js
@@ -4,6 +4,8 @@ import { useSelector, useDispatch } from 'react-redux'
 import { Button } from 'react-native-elements'
 import Colors from '../../styles/Colors'
 
+import { BACKEND_URL } from '../../env.config'
+
 export default function ButtonSection() {
     
     const dispatch = useDispatch()
@@ -12,9 +14,33 @@ export default function ButtonSection() {
     const removeRestaurantFromList = () => {
         dispatch({type:'REMOVE_RESTAURANT', restaurant: currentRestaurant})
     }
+
+    const addRestaurantToLiked = () => {
+        const likedRestaurant = {
+            yelp_id: currentRestaurant.id,
+            name: currentRestaurant.name,
+            party_id: 1,
+            user_id: 2
+        }
+
+        fetch(`${BACKEND_URL}/users`)
+        //     .then(response => response.json())
+        //     .then(console.log)
+        // // fetch(`${BACKEND_URL}/liked-restaurants`, {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-type': 'application/json',
+        //         'Accept': 'application/json'
+        //     },
+        //     body: JSON.stringify(likedRestaurant)
+        // })
+        // .then(response => response.json())
+        //     .then(console.log)
+    }
       
     const handleLike = () => {
-        console.log('like')
+        removeRestaurantFromList()
+        addRestaurantToLiked()
     }
     
     const handleDislike = () => {

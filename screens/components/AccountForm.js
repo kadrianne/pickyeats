3009f1ac60e1c53964836d6de8d8d11db889555f
kadrianne <kristine.a.du@gmail.com>
2020-05-17 16:15:44 -0600

    Create account and post to backend.

--- screens/components/AccountForm.js
+++ screens/components/AccountForm.js
@@ -2,35 +2,84 @@ import React from 'react'
 import { StyleSheet, View } from 'react-native'
 import { Input, Button } from 'react-native-elements'
 import Colors from '../../styles/Colors'
+import useFormField from '../../hooks/useFormField'
 
 import { BACKEND_URL } from '../../env.config'
 
 export default function AccountForm({ type }) {
+    
+    const [ name, handleNameChange ] = useFormField('')
+    const [ phone, handlePhoneChange ] = useFormField('')
+    const [ email, handleEmailChange ] = useFormField('')
+    const [ password, handlePasswordChange ] = useFormField('')
+
+    const createAccount = () => {
+        const accountData = { name, phone, email, password}
+
+        fetch(`${BACKEND_URL}/users/`, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(accountData)
+        }).then(console.log)
+
+    }
+
+    const loginUser = () => {
+        const loginData = { email, password }
+
+        fetch(`${BACKEND_URL}/login`, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(loginData)
+        })
+    }
+
+    const handleSubmit = () => {
+        if (type === 'signup') {
+            createAccount()
+        } else if (type === 'login') {
+            loginUser()
+        }
+    }
 
     return (
         <View style={styles.form}>
             {type === 'signup' ? <Input
-                placeholder='First Name'
+                placeholder='Full Name'
                 inputContainerStyle={{...styles.field, paddingHorizontal: 2}}
                 leftIcon={{ type: 'font-awesome', name: 'user', size: 24, color: Colors.darkOrange }}
+                value={name}
+                onChangeText={handleNameChange}
             /> : null }
             {type === 'signup' ? <Input
                 placeholder='Phone Number'
                 inputContainerStyle={styles.field}
                 leftIcon={{ type: 'font-awesome', name: 'phone', size: 24, color: Colors.darkOrange }}
+                value={phone}
+                onChangeText={handlePhoneChange}
             /> : null }
             <Input
                 placeholder='Email'
                 inputContainerStyle={styles.field}
                 leftIcon={{ type: 'font-awesome', name: 'envelope', size: 20, color: Colors.darkOrange }}
+                value={email}
+                onChangeText={handleEmailChange}
             />
             <Input
-                type='password'
+                secureTextEntry={true}
                 placeholder='Password'
                 inputContainerStyle={{...styles.field, paddingHorizontal: 3}}
                 leftIcon={{ type: 'font-awesome', name: 'lock', size: 24, color: Colors.darkOrange }}
+                value={password}
+                onChangeText={handlePasswordChange}
             />
-            <Button buttonStyle={styles.button} titleStyle={styles.buttonText} title='SUBMIT' />
+            <Button buttonStyle={styles.button} titleStyle={styles.buttonText} title='SUBMIT' onPress={handleSubmit} />
         </View>
     )
 }

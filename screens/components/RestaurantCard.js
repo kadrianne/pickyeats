2d48f4e0b4be78548e92dff144d35f92fa1a39b6
kadrianne <kristine.a.du@gmail.com>
2020-05-18 20:09:17 -0600

    Fetch reviews/more info and add styling.

--- screens/components/RestaurantCard.js
+++ screens/components/RestaurantCard.js
@@ -1,14 +1,18 @@
-import React, { useEffect } from 'react'
-import { StyleSheet, View, Text, Image } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, ScrollView, View, Text, Image } from 'react-native'
 import { useSelector } from 'react-redux'
 import { Button } from 'react-native-elements'
 import Colors from '../../styles/Colors'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import ButtonSection from './ButtonSection'
+import { YELP_API_TOKEN } from '../../env.config'
 
 export default function RestaurantCard() {
 
     const restaurant = useSelector(state => state.currentRestaurant)
+    const [ showMoreInfo, setShowMoreInfo ] = useState(false)
+    const [ restaurantInfo, setRestaurantInfo ] = useState([])
+    const [ reviews, setReviews ] = useState([])
 
     const renderCategories = () => {
         const categories = restaurant.categories.map(category => {
@@ -27,13 +31,61 @@ export default function RestaurantCard() {
         }
     }
 
+    const getMoreInfo = () => {
+        fetch(`https://api.yelp.com/v3/businesses/${restaurant.id}`, {
+            headers: {
+                'Authorization': `Bearer ${YELP_API_TOKEN}`
+            }
+        })
+            .then(response => response.json())
+            .then(results => setRestaurantInfo(results))
+    }
+
+    const getReviews = () => {
+        fetch(`https://api.yelp.com/v3/businesses/${restaurant.id}/reviews`, {
+            headers: {
+                'Authorization': `Bearer ${YELP_API_TOKEN}`
+            }
+        })
+            .then(response => response.json())
+            .then(results => setReviews(Array.from(results.reviews)))
+    }
+
+    const handleViewMore = () => {
+       setShowMoreInfo(previousState => !previousState)
+    }
+
+    useEffect(() => {
+        if (showMoreInfo === false) {
+            getMoreInfo()
+            getReviews()
+        }
+    },[showMoreInfo])
+
+    const getHours = () => {
+        // hours = restaurantInfo.hours
+        // console.log(restaurantInfo.hours)
+    }
+
+    const displayReviews = () => {
+        return reviews.map(review => {
+            return (
+                <View>
+                    <Text style={styles.text}>{review.user.name}</Text>
+                    <Text style={styles.text}>{review.text}</Text>
+                </View>
+            )
+        })
+    }
+
     return (
         <>
-        <View style={styles.card}>
+        <View style={showMoreInfo === false ? styles.card : styles.cardExpanded}>
             <Image
                 source={{uri: `${restaurant.image_url}`}} 
                 style={styles.image}
                 />
+            <ScrollView contentContainerStyle={showMoreInfo === false ? styles.cardInfo : null}>
             <Text style={styles.title}>{restaurant.name}</Text>
             <View style={styles.infoView}>
                 <View style={styles.ratingView}>
@@ -53,7 +105,8 @@ export default function RestaurantCard() {
                 }
             </View>
             <Text style={styles.categoryText}>{renderCategories()}</Text>
-            <Button
+            { showMoreInfo === false 
+            ? <Button
                 buttonStyle={styles.button} titleStyle={styles.buttonText} type='outline'
                 icon={{
                     name: 'arrow-downward',
@@ -61,9 +114,29 @@ export default function RestaurantCard() {
                     color: Colors.burgundy
                 }}
                 iconRight
-                title='VIEW MORE' />
+                title='VIEW MORE'
+                onPress={handleViewMore}
+                /> 
+            : <>
+                <Text style={styles.text}>{restaurant.display_phone}</Text>
+                <Text >Reviews: {restaurant.review_count}</Text>
+                {displayReviews()}
+                {/* <Text >{restaurantInfo.hours}</Text> */}
+                <Button
+                    buttonStyle={styles.button} titleStyle={styles.buttonText} type='outline'
+                    icon={{
+                        name: 'arrow-upward',
+                        size: 30,
+                        color: Colors.burgundy
+                    }}
+                    iconRight
+                    title='SHOW LESS'
+                    onPress={handleViewMore}
+                /> 
+            </> }
+            </ScrollView>
         </View>
-        <ButtonSection />
+        { showMoreInfo === false ? <ButtonSection /> : null }
         </>
     )
 }
@@ -82,6 +155,15 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.23,
         shadowRadius: 2.62,
         elevation: 4,
+    },
+    cardInfo: {
+        marginTop: 5,
+        height: '100%',
+        justifyContent: 'space-between'
+    },
+    cardExpanded: {
+        backgroundColor: Colors.orange,
+        height: '92%',
         justifyContent: 'space-between'
     },
     image: {

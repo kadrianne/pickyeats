c48d1c56cd8a5e4f2d2716774743fe043afdae06
kadrianne <kristine.a.du@gmail.com>
2020-05-22 17:51:19 -0600

    Add action for activating party.

--- reducers/ActivePartyReducer.js
+++ reducers/ActivePartyReducer.js
@@ -2,6 +2,8 @@ export default function activePartyReducer(state={},action) {
     switch (action.type) {
         case 'SET_PARTY': 
             return action.party
+        case 'ACTIVATE_PARTY':
+            return {...state, active: true}
         default: 
             return state
     }
